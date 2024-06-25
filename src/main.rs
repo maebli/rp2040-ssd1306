8517c3e216e648877c224cdbae490e31daf37de4
heroichornet <schnipper@gmail.com>
2024-06-26 00:23:06 +0200

    first attempt to get the dipslay running

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,20 @@
 #![no_std]
 #![no_main]
 
+use core::fmt::Write;
+
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_rp::gpio;
+use embassy_rp::{
+    gpio,
+    i2c::{self, Config},
+};
 use embassy_time::Timer;
 use gpio::{Level, Output};
+use ssd1306::{
+    mode::DisplayConfig, rotation::DisplayRotation, size::DisplaySize128x64, I2CDisplayInterface,
+    Ssd1306,
+};
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
@@ -17,11 +26,26 @@ async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
     let mut led = Output::new(p.PIN_25, Level::Low);
 
+    info!("set up i2c ");
+    let sda = p.PIN_0;
+    let scl = p.PIN_1;
+
+    let i2c = i2c::I2c::new_blocking(p.I2C0, scl, sda, Config::default());
+
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display =
+        Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0).into_terminal_mode();
+
+    display.init().unwrap();
+    let _ = display.clear();
+
     loop {
+        let _ = display.write_str("led on");
         info!("led on!");
         led.set_high();
         Timer::after_secs(1).await;
 
+        let _ = display.write_str("led off");
         info!("led off!");
         led.set_low();
         Timer::after_secs(1).await;
